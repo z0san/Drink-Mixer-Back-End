9d31da998650974a406c24de15b711fa8c195008
zohar <injapanthree@gmail.com>
2021-04-27 05:33:15 +0100

    working bby

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import SerialPort from "serialport";
-const path = "/dev/ttyS0";
+const local_path = "/dev/ttyACM0";
+const usb_path = "/dev/ttyUSB0";
 const Readline = SerialPort.parsers.Readline;
-const serial_port = new SerialPort(path);
-const parser = new Readline({ delimiter: "!" });
+const usb_serial_port = new SerialPort(usb_path);
+const local_serial_port = new SerialPort(local_path);
+const usb_parser = new Readline({ delimiter: "!" });
+const local_parser = new Readline({ delimiter: "\n" });
 import express from "express";
 import { Request, Response } from "express";
 const app = express();
@@ -14,27 +17,33 @@ const serial_handler = (data: string) => {
 	for (let char = 0; char < data.length; char++) {
 		if (data.charCodeAt(char) !== 65533) nice_string += data[char];
 	}
-	console.log("handling: " + nice_string);
-	serial_port.write(data);
+	// console.log("handling: " + nice_string);
+	local_serial_port.write(nice_string);
 };
 
-serial_port.pipe(parser);
-parser.on("data", serial_handler);
+usb_serial_port.pipe(usb_parser);
+usb_parser.on("data", serial_handler);
 
-app.post("/", (req: Request, res: Response) => {
-	let body = req.body;
-	if (body.type == "motor") {
-		console.log("got motor request");
-		let motor = body.motor;
-		let time = body.time;
-		let start_string = "pmotor " + String(motor) + " " + "on\n";
-		let end_string = "pmotor " + String(motor) + " " + "off\n";
-		serial_port.write(start_string);
-		setTimeout(() => serial_port.write(end_string), time);
-	}
-	res.send("Hello World!");
-});
+local_serial_port.pipe(local_parser);
+// local_parser.on("data", console.log);
 
-app.listen(port, () =>
-	console.log(`Example app listening at http://localhost:${port}`)
+ app.post("/", (req: Request, res: Response) => {
+ 	let body = req.body;
+
+ 	if (body.type == "motor") {
+ 		console.log("got motor request");
+ 		let motor = body.motor;
+ 		let time = body.time;
+ 		let start_string = "pmotor " + String(motor) + " " + "on\n";
+ 		let end_string = "pmotor " + String(motor) + " " + "off\n";
+ 		local_serial_port.write(start_string);
+ 		setTimeout(() => local_serial_port.write(end_string), time);
+ 	}
+ 	res.send("Hello World!");
+ });
+
+ app.listen(port, () =>
+ 	console.log(`Example app listening at http://localhost:${port}`)
 );
+
+console.log("ready");
