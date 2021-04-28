38fd2118767885b15f9c4db3ba8e51fb0c8c6b08
zohar <injapanthree@gmail.com>
2021-04-28 01:51:54 +0100

    working

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,15 @@ const usb_parser = new Readline({ delimiter: "!" });
 const local_parser = new Readline({ delimiter: "\n" });
 import express from "express";
 import { Request, Response } from "express";
+
+import custom_drinks from "./Drinks.json";
+
+import cors from "cors";
+
+
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 const port = 3000;
 
@@ -17,7 +25,7 @@ const serial_handler = (data: string) => {
 	for (let char = 0; char < data.length; char++) {
 		if (data.charCodeAt(char) !== 65533) nice_string += data[char];
 	}
-	// console.log("handling: " + nice_string);
+	console.log("handling: " + nice_string);
 	local_serial_port.write(nice_string);
 };
 
@@ -25,25 +33,38 @@ usb_serial_port.pipe(usb_parser);
 usb_parser.on("data", serial_handler);
 
 local_serial_port.pipe(local_parser);
-// local_parser.on("data", console.log);
+local_parser.on("data", (data: string) => console.log("fron arduino: " + data));
 
- app.post("/", (req: Request, res: Response) => {
- 	let body = req.body;
+app.post("/", (req: Request, res: Response) => {
+	let body = req.body;
 
- 	if (body.type == "motor") {
- 		console.log("got motor request");
- 		let motor = body.motor;
- 		let time = body.time;
- 		let start_string = "pmotor " + String(motor) + " " + "on\n";
- 		let end_string = "pmotor " + String(motor) + " " + "off\n";
- 		local_serial_port.write(start_string);
- 		setTimeout(() => local_serial_port.write(end_string), time);
- 	}
- 	res.send("Hello World!");
- });
+	if (body.type == "motor") {
+		console.log("got motor request");
+		console.debug(body);
+		let motor = body.motor;
+		if (body.time) {
+			let time = body.time;
+			let start_string = "pmotor " + String(motor) + " " + "on\n";
+			let end_string = "pmotor " + String(motor) + " " + "off\n";
+			local_serial_port.write(start_string);
+			setTimeout(() => local_serial_port.write(end_string), time);
+		} else if (body.action) {
+			let action = body.action;
+			let command_string = "pmotor " + String(motor) + " " + action + "\n";
+			local_serial_port.write(command_string);
+		}
+	}
+	res.send("Success!");
+});
 
- app.listen(port, () =>
- 	console.log(`Example app listening at http://localhost:${port}`)
+
+app.get("/custom", (req: Request, res: Response) => {
+	res.send(custom_drinks);
+}
+
+
+app.listen(port, () =>
+console.log(`Example app listening at http://localhost:${port}`)
 );
 
 console.log("ready");
